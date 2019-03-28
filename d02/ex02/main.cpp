0657317920c243f42d57f397195a81a8e81fc36a
Paul BIE <pbie@e2r2p5.42.fr>
2019-03-28 14:05:43 +0100

    [Add] - D02 EX02 more thorough tests

--- d02/ex02/main.cpp
+++ d02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pbie <pbie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 16:16:56 by pbie              #+#    #+#             */
-/*   Updated: 2019/03/19 18:43:48 by pbie             ###   ########.fr       */
+/*   Updated: 2019/03/28 14:02:39 by pbie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,29 @@ int main(void)
 	std::cout << "b " << b << std::endl;
 
 	std::cout << "max(a, b): " << Fixed::max( a, b ) << std::endl;
+	std::cout << "min(a, b): " << Fixed::min( a, b ) << std::endl;
 
 	Fixed testA = Fixed(5);
 	Fixed testB = Fixed(5);
 	Fixed testC = Fixed(5.5f);
 	Fixed testD = Fixed(10);
 
-	std::cout << "testA == testB: " << ((testA == testB) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA == testC: " << ((testA == testC) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA != testB: " << ((testA != testB) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA != testC: " << ((testA != testC) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA > testB: " << ((testA > testB) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA >= testB: " << ((testA >= testB) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA <= testC: " << ((testA <= testC) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA < testC: " << ((testA < testC) == 0 ? "false" : "true") << std::endl;
-	std::cout << "testA + testC: " << (testA + testC) << std::endl;
-	std::cout << "testA + testB: " << (testA + testB) << std::endl;
-	std::cout << "testD - testB: " << (testD - testB) << std::endl;
-	std::cout << "testD - testC: " << (testD - testC) << std::endl;
+	std::cout << testA << " == " << testB << "   : " << ((testA == testB) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " == " << testC << " : " << ((testA == testC) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " != " << testB << "   : " << ((testA != testB) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " != " << testC << " : " << ((testA != testC) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " >  " << testB << "   : " << ((testA > testB) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " >= " << testB << "   : " << ((testA >= testB) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " <= " << testC << " : " << ((testA <= testC) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " <  " << testC << " : " << ((testA < testC) == 0 ? "false" : "true") << std::endl;
+	std::cout << testA << " +  " << testC << " : " << (testA + testC) << std::endl;
+	std::cout << testA << " +  " << testB << "   : " << (testA + testB) << std::endl;
+	std::cout << testD << " - " << testB << "   : " << (testD - testB) << std::endl;
+	std::cout << testD << " - " << testC << " : " << (testD - testC) << std::endl;
+	std::cout << testD << " / " << testB << "   : " << (testD / testB) << std::endl;
+	std::cout << testD << " / " << testC << " : " << (testD / testC) << std::endl;
+	std::cout << testD << " * " << testB << "   : " << (testD * testB) << std::endl;
+	std::cout << testD << " * " << testC << " : " << (testD * testC) << std::endl;
 
 	return (0);
 }
