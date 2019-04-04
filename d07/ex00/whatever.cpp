f44daa99b6966cd049dfa3344156fbf4b77e1dd0
Paul BIE <pbie@e2r10p4.42.fr>
2019-04-04 18:00:02 +0200

    [Fix] - Can now swap classes

--- d07/ex00/whatever.cpp
+++ d07/ex00/whatever.cpp
@@ -6,12 +6,32 @@
 /*   By: pbie <pbie@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 15:32:53 by pbie              #+#    #+#             */
-/*   Updated: 2019/04/04 15:42:20 by pbie             ###   ########.fr       */
+/*   Updated: 2019/04/04 17:59:17 by pbie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+
+class Test {
+
+
+	public:
+		Test( int n ) : _n( n ) {}
+		bool operator==( Test const & rhs ) { return (this->_n == rhs._n); }
+		bool operator!=( Test const & rhs ) { return (this->_n != rhs._n); }
+		bool operator>( Test const & rhs ) { return (this->_n > rhs._n); }
+		bool operator<( Test const & rhs ) { return (this->_n < rhs._n); }
+		bool operator>=( Test const & rhs ) { return (this->_n >= rhs._n); }
+		bool operator<=( Test const & rhs ) { return (this->_n <= rhs._n); }
+		int get() const { return _n; }
+
+	private:
+		int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Test const & rhs ) { o << rhs.get(); return o; }
+
 
 template <typename T>
 T max(T a, T b)
@@ -22,8 +42,7 @@ T max(T a, T b)
 template <typename T>
 void swap(T &a, T &b)
 {
-	T temp;
-	temp = a;
+	T temp(a);
 	a = b;
 	b = temp;
 }
@@ -47,5 +66,11 @@ int main( void ) {
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	Test six(6);
+	Test seven(7);
+	::swap( six, seven );
+	std::cout << "six = " << six << ", seven = " << seven << std::endl;
+	std::cout << "min( six, seven ) = " << ::min( six, seven ) << std::endl;
+	std::cout << "max( six, seven ) = " << ::max( six, seven ) << std::endl;
 	return 0;
 }
